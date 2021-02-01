8e705aefdd00e5f5aa0c982466b5138f4c1dd75a
Kim Ki Bum <55014424+rlqja1107@users.noreply.github.com>
2021-02-01 17:08:38 +0900

    Sorec

--- Recommender_System/Sorec/Main_Sorec.py
+++ Recommender_System/Sorec/Main_Sorec.py
@@ -3,7 +3,7 @@ from Sorec import Sorec
 config={'lamb_c':10,
        'lambda_':0.001,
        "n_user":49290,
-       'n_item':139739,
+       'n_item':139739, 
        'latent_dim':10,
        'test_size':0.01,
        'lr':0.1,
