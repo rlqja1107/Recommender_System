f6fde1bff4e76f702dfba08fab19d64b88c62cec
2016032897 <rlqja1107@hanyang.ac.kr>
2021-02-03 16:17:09 +0900

    node2vec

--- Graph/node2vec/node2vec.py
+++ Graph/node2vec/node2vec.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import sys
-sys.path.append('..')
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('__file__'))))
 from dataset import load_graph
 import random
 import numpy as np
