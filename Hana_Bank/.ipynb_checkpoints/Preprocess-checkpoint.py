80c3afa9c23c235b97b9dc3c3339cf332102e695
KiBum <rlqja9611@naver.com>
2021-02-07 16:49:38 +0900

    remove ignore

--- Hana_Bank/.ipynb_checkpoints/Preprocess-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-import sys
-os.path.join('/home/kibum/recommender_system/Hana_Bank')
-sys.path.append(os.path.abspath(os.path.dirname('__file__'))+'/Hana_Bank')
-data = pd.read_excel(os.getcwd()+'/Hana_Bank/Hana_Data.xlsx')
-data.head()
